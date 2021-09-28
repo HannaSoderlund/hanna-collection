d643265159e7629c387bae6477a0e23f98461062
HannaSoderlund <hanna.ej.soderlund@gmail.com>
2021-09-28 12:27:08 +0100

    set up the database connection and retrieved the contents of the database

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+// create a PDO connection
+$db = new PDO('mysql:host=db; dbname=hanna-collection', 'root', 'password');
+
+// Write the SQL query to retrieve all the collection items
+$query = $db->prepare("SELECT * FROM `vegetables`");
+$query->execute();
+$results = $query->fetchAll();
+
+//Create a loop over the results to echo out the various stats for each item
+foreach($results as $vegetable) {
+    echo $vegetable['id'] . ' - ' .
+        $vegetable['common-name'] . ' - ' .
+        $vegetable['cultivar-name'] . ' - ' .
+        $vegetable['family'] . ' - ' .
+        $vegetable['likes'] . ' - ' .
+        $vegetable['dislikes'] . ' - ' .'<br>';
+}
