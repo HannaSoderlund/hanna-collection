f5fe902a563dc2d19c1f50a631c35d8caaec536e
HannaSoderlund <hanna.ej.soderlund@gmail.com>
2021-09-29 16:34:24 +0100

    created a form

--- validation.php
+++ validation.php
@@ -3,8 +3,13 @@
 $commonName = $_POST['common-name'];
 $cultivarName = $_POST['cultivar-name'];
 $family = $_POST['family'];
-$family = $_POST['likes'];
-$family = $_POST['dislikes'];
-$family = $_POST['image'];
+$likes = $_POST['likes'];
+$dislikes = $_POST['dislikes'];
+$image = $_POST['image'];
 echo 'This is where your data goes for now!';
 echo $commonName;
+echo $cultivarName;
+echo $family;
+echo $likes;
+echo $dislikes;
+echo $image;
